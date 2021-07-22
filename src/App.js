67aa524d77bdd178f04f84be426dae806c0ccf87
himanshulal9 <himanshulal9@gmail.com>
2021-07-22 15:00:41 +0530

    starter code

--- src/App.js
+++ src/App.js
@@ -1,13 +1,9 @@
 import "./App.css";
-// import ClassComponent from "./components/ClassComponent";
-import FunctionalComponent from "./components/FunctionalComponent";
 
 function App() {
   return (
     <div>
-      Hello Greatcoders
-      {/* <ClassComponent /> */}
-      <FunctionalComponent />
+      <h5>Hello Greatcoders</h5>
     </div>
   );
 }
